1ebed78d2c749aa7830b21f0f83d767d66d67125
Vicente Ruiz <vruiz2.0@gmail.com>
2012-02-02 11:27:06 +0100

    Basic example of signals

--- examples/signals/basic.py
+++ examples/signals/basic.py
@@ -1,44 +1,33 @@
 from subscription.signals import Signal
-from subscription.signals.accumulators import SumAccumulator
-
-def handler1(param):
-    print('Handler1:', param)
-    return 1
-
-def handler2(param):
-    print('Handler2:', param)
-    return 2
-
-mysignal = Signal()
-mysignal.connect(handler1)
-
-print('Emit')
-val = mysignal.emit(1)
-print('Return value:', val) # Must be 'None'
-
-print('Emit')
-mysignal.connect(handler2)
-mysignal.emit(2)
-
-print('Emit')
-mysignal.disconnect(handler1)
-mysignal.emit(3)
-
-print('Emit')
-mysignal.connect(handler1)
-mysignal.connect(handler1)
-mysignal.disconnect(handler2)
-mysignal.connect(handler2)
-mysignal.emit(4)
-
-print('Emit')
-mysignal.clear()
-mysignal.emit(5)
 
 
-print('Sum accumulator')
-mysignal = Signal(accumulator=SumAccumulator())
-mysignal.connect(handler1)
-mysignal.connect(handler2)
-val = mysignal.emit('sum')
-print('Return value:', val) # Must be sum of all handlers
+total_tentacles = 0
+
+
+def inform_of_frobnicator(frobnicator_type, num_tentacles):
+    print ("A frobnicator of type '%s' with %d tentacles has arrived"
+           % (frobnicator_type, num_tentacles))
+
+
+def count_tentacles(frobnicator_type, num_tentacles):
+    global total_tentacles
+    total_tentacles += num_tentacles
+    print ('Total tentacles so far: %s' % total_tentacles)
+
+
+def run_if_scary(frobnicator_type, num_tentacles):
+    if frobnicator_type == 'Slimy':
+        print('I panic')
+        import sys
+        sys.exit ()
+
+
+new_frobnicator = Signal()
+new_frobnicator.connect(inform_of_frobnicator)
+new_frobnicator.connect(count_tentacles)
+new_frobnicator.connect(run_if_scary)
+
+new_frobnicator('Friendly', 8)
+new_frobnicator('Sleepy', 4)
+new_frobnicator('Slimy', 12)
+new_frobnicator('Hungry', 6)
